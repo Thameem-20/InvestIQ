bc11416fffe32c36b461346e15652dceb507ecd0
mausooq <mauchoomausooq051@gmail.com>
2024-07-14 22:15:57 +0530

    getting started

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className="bg-orange-500 flex-1 justify-center items-center ">
+      <Text className='color-white text-3xl font-semibold'>Mausooq</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ src/components/Avtar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Avtar = () => {
+  return (
+    <View>
+      <Text>Avtar</Text>
+    </View>
+  )
+}
+
+export default Avtar
