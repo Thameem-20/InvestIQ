48626f2823ad5275c132d7af64977f775b95aa4a
mausooq <mauchoomausooq051@gmail.com>
2024-07-15 23:16:25 +0530

    app.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [session,setSession] = useState();
   return (
     <View className="bg-orange-500 flex-1 justify-center items-center ">
-      <Text className='color-white text-3xl font-semibold'>Mausooq</Text>
+      <Text className='color-white text-3xl font-semibold'>Muskan </Text>
     </View>
   )
 }
