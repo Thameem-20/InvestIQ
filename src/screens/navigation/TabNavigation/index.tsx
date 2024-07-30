73103c0f4d84aec8f1d1c05d8c17e14a44e4b4b9
mausooq <mauchoomausooq051@gmail.com>
2024-07-30 21:00:00 +0530

    'taba-navigation'

--- src/screens/navigation/TabNavigation/index.tsx
+++ src/screens/navigation/TabNavigation/index.tsx
@@ -1,15 +1,53 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
-import { createButtonTabNavigator } from '@react-navigation/bottom-tabs'
-import { icons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons';
+import HomeNavigation from './HomeNavigation';
+import MarketNavigation from './MarketNavigation';
+import SearchNavigation from './SearchNavigation';
+import NewsNavigation from './NewsNavigation';
+import ProfileNavigation from './ProfileNavigation';
 
-const Tab = createButtonTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.navigation>
-      
-    </Tab.navigation>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      headerShown:false,
+      tabBarIcon : ({ focused }) => {
+        let iconName;
+        if(route.name === 'Home'){
+          iconName ="home"
+        }else if(route.name === 'Market'){
+          iconName = 'stats-chart-outline'
+        }else if(route.name === 'Search'){
+          iconName = 'search-outline'
+        }
+        else if(route.name === 'News'){
+          iconName = 'newspaper-outline'
+        }
+        else if(route.name === 'Profile'){
+          iconName = 'person-outline'
+        }
+
+        const customizeSize =25;
+        return (
+          <Ionicons 
+          name={iconName}
+          size = {customizeSize}
+          color={(focused ? '#2ab07c' :"gray" )}
+          />
+        )
+      }
+    })}
+    >
+      <Tab.Screen name='Home' component={HomeNavigation}/>
+      <Tab.Screen name='Market' component={MarketNavigation}/>
+      <Tab.Screen name='Search' component={SearchNavigation}/>
+      <Tab.Screen name='News' component={NewsNavigation}/>
+      <Tab.Screen name='Profile' component={ProfileNavigation}/>
+    </Tab.Navigator>
   );
 }
 
